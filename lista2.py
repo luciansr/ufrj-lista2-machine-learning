424e5b2bed97790cf025d1d029ac00dba633e758
lucian.rodrigues <lucian.rodrigues@stoneage.com.br>
2017-10-30 14:24:57 -0200

    criando funcoes para transformar em lista de vetores

--- lista2.py
+++ lista2.py
@@ -1,5 +1,20 @@
+def getVectorFromFile(fileName):
+    file_object = open(fileName, 'r')
+
+    lista = file_object.readlines()
+    result = map(float, lista)
+
+    return result
+
+
 def main():
+    file1a = 'file-l2-p-1a.txt'
+    file1b = 'file-l2-p-1b.txt'
+    file1a = 'file-EM.txt'
     print('teste')
+    resultado = getVectorFromFile(file1a)
+
+    print(resultado)
 
 
 main()
